7b751f2092a7938d80d93a4dd37b83708a41abc7
hemadri-git <bathulahemadri@gmail.com>
2024-10-17 21:01:51 +0530

    oop

--- src/com/programs/oop2/RectangleTest.java
+++ src/com/programs/oop2/RectangleTest.java
@@ -4,7 +4,7 @@ public class RectangleTest {
 
 	public static void main(String[] args) {
 		
-		Rectangle rectangle = new Rectangle(5, -10);
+		Rectangle rectangle = new Rectangle(5, 10);
 		
 		double area = rectangle.getArea();
 		double parameter = rectangle.getParameter();
